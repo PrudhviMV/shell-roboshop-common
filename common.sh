99f9cf1fd2e1d054a7a9141f9b38328019704290
PrudhviMV <prudhvimenta@gmail.com>
2026-06-05 17:03:09 +0530

    Adding shipping changes

--- common.sh
+++ common.sh
@@ -12,6 +12,7 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
 SCRIPT_DIR=$PWD
 Mongo_host=mongodb.prudhvii.fun
+MYSQL_HOST=mysql.prudhvii.fun
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE

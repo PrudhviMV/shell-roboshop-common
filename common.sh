0259709b7363d52e93cf8d70d8c0e734d59e3dac
PrudhviMV <prudhvimenta@gmail.com>
2026-06-05 12:53:16 +0530

    Adding catalogue

--- common.sh
+++ common.sh
@@ -11,6 +11,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
 SCRIPT_DIR=$PWD
+Mongo_host=mongodb.prudhvii.fun
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE

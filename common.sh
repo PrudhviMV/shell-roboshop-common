4c060cac630d53b8a04a38a93b553ba72d65948e
PrudhviMV <prudhvimenta@gmail.com>
2026-06-05 10:18:50 +0530

    Adding common and mongo files

--- common.sh
+++ common.sh
@@ -34,7 +34,7 @@ check_root(){
     fi
 }
 
-(){
+print_total_time(){
     END_TIME=$(date +%s)
     TOTAL_TIME=$(( $END_TIME - $START_TIME ))
     echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
